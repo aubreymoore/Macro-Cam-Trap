976b9f3bc422cd1eb9c16efd5885064c01933c93
Aubrey Moore <aubreymoore@guam.net>
2019-07-17 20:25:39 +1000

    minors

--- draw_bounding_boxes.py
+++ draw_bounding_boxes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[60]:
+# In[1]:
 
 
 import pandas as pd
@@ -10,12 +10,12 @@ import argparse
 import sys
 
 
-# In[61]:
+# In[2]:
 
 
 # The following line allows testing argparse within a Jupyter notebook
 # Comment it out when using code in a script
-#sys.argv = ['draw_bounding_boxes', '/media/aubrey/70D7-5135/videos', '1563341307489']
+#sys.argv = ['draw_bounding_boxes', '/media/pi/70D7-5135/videos', '1563341307489']
 
 parser = argparse.ArgumentParser(description='Description goes here.')
 
@@ -36,27 +36,28 @@ parser.add_argument('--delay_ms',
 args = parser.parse_args()
 
 
-# In[62]:
+# In[3]:
 
 
 # Load bounding_boxes.csv into a data frame.
 fn = '{}/{}/bounding_boxes.csv'.format(args.data_dir, args.video_timestamp)
+#print(fn)
 df = pd.read_csv(fn)
 #df
 
 
-# In[63]:
+# In[4]:
 
 
 # Get dimensions of first frame, we assume that all frames have the same size.
-path = '{}/{}'.format(mydir, df['filename'][0])
+path = '{}/{}/{}'.format(args.data_dir, args.video_timestamp, df['filename'][0])
 #print(path)
 img = cv2.imread(path)
 frame_height, frame_width, _ = img.shape
 #print('frame_height: {}, frame_width: {}'.format(frame_height, frame_width))
 
 
-# In[64]:
+# In[5]:
 
 
 # Convert coordinates from proportions to pixels
@@ -64,21 +65,24 @@ df['xtl'] = df['xtl'].apply(lambda x: int(x * frame_width))
 df['ytl'] = df['ytl'].apply(lambda x: int(x * frame_height))
 df['xbr'] = df['xbr'].apply(lambda x: int(x * frame_width))
 df['ybr'] = df['ybr'].apply(lambda x: int(x * frame_height))
+df
 
 
-# In[65]:
+# In[6]:
 
 
 # Group by frame
 grouped = df.groupby('filename')
 
 
-# In[66]:
+# In[11]:
 
 
 # Iterate over frames
 for name, df_group in grouped:
-    img = cv2.imread(name, cv2.IMREAD_UNCHANGED)
+    path = '{}/{}/{}'.format(args.data_dir, args.video_timestamp, name)
+    #print(path)
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     
     # Draw bounding boxes on each frame
     for index, row in df_group.iterrows():
@@ -86,13 +90,13 @@ for name, df_group in grouped:
         cv2.rectangle(img, (xtl, ytl), (xbr, ybr), (0, 255, 0), 3)
 
     # Show image with bounding boxes
-    cv2.imshow(mydir, img)
+    cv2.imshow(args.video_timestamp, img)
     cv2.waitKey(args.delay_ms)
 
 cv2.destroyAllWindows()
 
 
-# In[52]:
+# In[ ]:
 
 
 #get_ipython().system("jupyter nbconvert --to=script 'draw_bounding_boxes.ipynb'")
